2802ed75e17f9018523b814cd57391c017858153
Morgan <heinemann.morgan@gmail.com>
2020-03-03 13:07:17 -0800

    schema updated, formated translators

--- schema.sql
+++ schema.sql
@@ -2,17 +2,18 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS locations;
 
 CREATE TABLE locations (
-    id SERIAL,
-    api_name VARCHAR(255),
-    location_name VARCHAR(255),
+    id SERIAL PRIMARY KEY,
+    city_name VARCHAR(255),
+    date_stamp NUMERIC(10),
     latitude DECIMAL(9,6),
     longitude DECIMAL(9,6),
-    percipitation DECIMAL(3,1),
+    temperature DECIMAL(4,1),
     humidity DECIMAL(3,1),
     wind_speed DECIMAL(4,1),
-    wind_direction DECIMAL(3),
+    wind_direction NUMERIC(3),
     description VARCHAR(255),
-    date_modified DATE
+    api_name VARCHAR(255),
+    date_retrieved VARCHAR(255)
     );
 
 SELECT * FROM locations;
