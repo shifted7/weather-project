80e13c72441efeda0fbe91c0966059272b815389
spacecowboi <cunningham2192@gmail.com>
2020-03-04 14:57:39 -0800

    got cards rendered to front page

--- public/js/app.js
+++ public/js/app.js
@@ -9,9 +9,25 @@ function getWeather(event){
   console.log(getWeatherInput);
 
   // add ajax call with query to '/'
-  $.ajax('')
-  $.ajax(`/today/?input=${getWeatherInput}`, {method:'GET', dataType:'JSON',})
-    .then(ajaxResponse => {
-      console.log('Ajax response recieved:', ajaxResponse);
+  $.ajax(`/darksky?input=${getWeatherInput}`, {method: 'GET', dataType:'JSON',})
+    .then(darkskyresponse =>{
+      // console.log('hello');
+      console.log(typeof(darkskyresponse));
+      // darkskyresponse = JSON.parse(darkskyresponse);
+      console.log('response', darkskyresponse);
+      let source = document.getElementById('entry-template').innerHTML;
+      console.log('1');
+      let template = Handlebars.compile(source);
+      console.log('2');
+      let card = template(darkskyresponse[0]);
+      console.log('3');
+      $('#forecast').append(card);
+      console.log('4');
+      console.log('Dark sky response recieved', darkskyresponse);
     });
+  // $.ajax(`/today/?input=${getWeatherInput}`, {method:'GET', dataType:'JSON',})
+  //   .then(ajaxResponse => {
+  //     console.log('Ajax response recieved:', ajaxResponse);
+  //   });
 }
+
